9604a109c3b81dd23a228496e33d63f963a8d14c
maciejziemichod <maciej.ziemichod@gmail.com>
2023-11-15 21:22:28 +0100

    feat: linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/strict-type-checked",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    root: true,
+    ignorePatterns: ["dist/*", "node_modules/*", "**/*.js"],
+};
